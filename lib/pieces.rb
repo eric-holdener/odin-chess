90a0f000a6b0e362091607afbc3285ce88b649aa
EHoldener <eholdener12@gmail.com>
2022-04-07 20:10:24 -0700

    initial class creation and creating spec files

---
+++ lib/pieces.rb
@@ -0,0 +1,20 @@
+class Pieces
+end
+
+class King < Pieces
+end
+
+class Queen < Pieces
+end
+
+class Knight < Pieces
+end
+
+class Rook < Pieces
+end
+
+class Bishop < Pieces
+end
+
+class Pawn < Pieces
+end
