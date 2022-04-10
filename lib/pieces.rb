998bb8d815a52e3088e383ab5e4a757e5548871d
eric <eholdener12@gmail.com>
2022-04-10 10:54:52 -0700

    added get valid moves for king and knight. Going to write tests for all pieces and add get valid moves for all pieces

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -13,6 +13,27 @@ class King < Pieces
     super(parent)
     @symbol = 'K'
   end
+
+  def get_valid_moves(node)
+    children = []
+    children.push([node[0] + 1, node[1] + 1])
+    children.push([node[0] + 1, node[1] + 0])
+    children.push([node[0] + 1, node[1] - 1])
+    children.push([node[0] + 0, node[1] + 1])
+    children.push([node[0] + 0, node[1] - 1])
+    children.push([node[0] - 1, node[1] + 1])
+    children.push([node[0] - 1, node[1] + 0])
+    children.push([node[0] - 1, node[1] - 1])
+    i = 0
+    while i < children.length
+      if children[i][0] > 7 || children[i][0].negative? || children[i][1] > 7 || children[i][1].negative?
+        children.delete_at(i)
+      else
+        i += 1
+      end
+    end
+    self.valid_moves = children
+  end
 end
 
 class Queen < Pieces
@@ -28,6 +49,27 @@ class Knight < Pieces
   def initialize(parent)
     super(parent)
     @symbol = 'H'
+  end
+
+  def get_valid_moves(node)
+    children = []
+    children.push([node[0] + 2, node[1] + 1])
+    children.push([node[0] + 2, node[1] - 1])
+    children.push([node[0] - 2, node[1] + 1])
+    children.push([node[0] - 2, node[1] - 1])
+    children.push([node[0] + 1, node[1] + 2])
+    children.push([node[0] + 1, node[1] - 2])
+    children.push([node[0] - 1, node[1] + 2])
+    children.push([node[0] - 1, node[1] - 2])
+    i = 0
+    while i < children.length
+      if children[i][0] > 7 || children[i][0].negative? || children[i][1] > 7 || children[i][1].negative?
+        children.delete_at(i)
+      else
+        i += 1
+      end
+    end
+    self.valid_moves = children
   end
 end
 
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -1,7 +1,7 @@
 require './lib/pieces.rb'
 
 describe Pieces do
-  subject(:pieces) { described_class.new }
+  subject(:pieces) { described_class.new('W') }
 
   describe '#initialize' do
     context 'initializes that master pieces class with the correct variables' do
@@ -17,7 +17,7 @@ describe Pieces do
 end
 
 describe King do
-  subject(:king) { described_class.new }
+  subject(:king) { described_class.new('W') }
 
   describe '#initalize' do
     context 'the subclass inherits from the master class' do
@@ -33,6 +33,17 @@ describe King do
     context 'the subclass has its symbol set' do
       it 'the king has symbol k' do
         expect(king.symbol).to eq('K')
+      end
+    end
+  end
+
+  describe '#valid_moves(location)' do
+    context 'returns a list of all valid moves a king can move from any location' do
+      it 'returns valid moves for king from location[0][0[' do
+        expected = [[1,1],[1,0],[0,1]]
+        node = [0, 0]
+        valid = king.get_valid_moves(node)
+        expect(valid).to eq(expected)
       end
     end
   end
